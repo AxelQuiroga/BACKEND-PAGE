5d985d57f2b58eeeb4ee62b556998d7025321fcd
Seby <lokuraa18@hotmail.com>
2026-02-28 19:33:52 -0300

    manejo id invalido

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,7 +5,16 @@ export const asyncHandler = (fn) => (req,res,next) =>{
 export const errorHandler = (err,req,res,next) =>{
     console.error(err)
 
-    res.status(err.statusCode || 500).json({
+    //manejar id invalido
+    if(err.name === "CastError" && err.kind === "ObjectId") {
+        return res.status(400).json({
+            success:false,
+            message:"Invalid ID format"
+        })
+    }
+
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({
         success: false,
         message: err.message || "Internal Server Error"
     })
